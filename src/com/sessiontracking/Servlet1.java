67b041e70bd9df8dace734bfa8c481a0b6c35ead
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-15 23:31:26 +0530

    Cookies (to manage state)
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ src/com/sessiontracking/Servlet1.java
@@ -0,0 +1,54 @@
+package com.sessiontracking;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+				
+		String name = request.getParameter("name");
+		out.println("<h1>Hello , "+name+ " welcome to my website...</h1>");
+		
+		out.println("<h1><a href='Servlet2'>Go To Servlet 2</a></h1>");
+		
+		//create a cookie
+		Cookie c = new Cookie("user_name", name);
+		response.addCookie(c);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
