4a8df8565f3de4214784f15ac19d8fc8825a2f7a
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-17 23:18:07 +0530

    URL Rewriting
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ src/com/myservlets/Servlet01.java
@@ -0,0 +1,38 @@
+package com.myservlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet01")
+public class Servlet01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Servlet01() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		
+		//name fetch.........
+		String name = request.getParameter("user_name");
+		
+		out.println("<h1> Your name is : "+name+"</h1>");
+		
+		out.println("<a href='Servlet02?user="+name+"'> Go to second servlet </a>");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/myservlets/Servlet02.java
@@ -0,0 +1,35 @@
+package com.myservlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet02")
+public class Servlet02 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Servlet02() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("<h1 style='color:green;'>Welcome to Servlet02</h1>");
+		
+		String name = request.getParameter("user");
+		out.println("<h1 style='color:red;'>Welcome back "+name+"</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
